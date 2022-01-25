0bc1c5ac8e3f79956bbb9685d8029748102ee41a
juan13ut <juan13u@gmail.com>
2022-01-25 15:33:52 +0000

    Choose a random post like the main post

--- applications/base/views.py
+++ applications/base/views.py
@@ -1,4 +1,5 @@
 import random
+from turtle import pos
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Post, Category, SocialMedias, Web, Suscriptor
@@ -6,5 +7,18 @@ from .models import Post, Category, SocialMedias, Web, Suscriptor
 
 class Inicio(ListView):
     
-      def get(self,request,*args,**kwargs):
-          return render(request, 'index.html')
+    def get(self,request,*args,**kwargs):
+        posts = list(Post.objects.filter(
+                status = True,
+                published = True
+                ).values_list('id', flat = True))
+        print(posts)
+        
+        main_post = random.choice(posts)
+        
+        main_post = Post.objects.get(id = main_post)
+        
+        print(main_post)
+        
+        
+        return render(request, 'index.html')
