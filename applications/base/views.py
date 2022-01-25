273c15723b7260cce82bb04f1ba5ded506265a57
juan13ut <juan13u@gmail.com>
2022-01-25 16:11:37 +0000

    render post secundaries

--- applications/base/views.py
+++ applications/base/views.py
@@ -6,6 +6,9 @@ from django.views.generic import ListView
 from .models import Post, Category, SocialMedias, Web, Suscriptor
 
 
+def getPostbyId(id):
+    return Post.objects.get(id = id)
+
 class Inicio(ListView):
     
     def get(self,request,*args,**kwargs):
@@ -17,12 +20,25 @@ class Inicio(ListView):
         
         main_post = random.choice(posts)
         
-        main_post = Post.objects.get(id = main_post)
+        main_post = getPostbyId(main_post)
+        
+        post1 = random.choice(posts)
+        posts.remove(post1)
+        post2 = random.choice(posts)
+        posts.remove(post2)
+        post3 = random.choice(posts)
+        posts.remove(post3)
+        post4 = random.choice(posts)
+        posts.remove(post4)
         
         print(main_post)
         
         context = {
             'main':  main_post,
+            'post1': getPostbyId(post1),
+            'post2': getPostbyId(post2),
+            'post3': getPostbyId(post3),
+            'post4': getPostbyId(post4),
         }        
         
         return render(request, 'index.html', context)
